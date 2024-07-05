3712d76b80abb5f16a32f77db2a8de0fcba28eba
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-07-05 13:46:14 +0100

    Car class created

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    public String model;
+    public String color;
+    public int seats;
+
+    public void display() {
+        System.out.println("Model is " + model);
+        System.out.println("Color is " + color);
+        System.out.println("Number of seats are " + seats);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Car carNumOne = new Car();
+        carNumOne.model = "Toyota Corolla AE 100";
+        carNumOne.color = "Grey";
+        carNumOne.seats = 4;
+        carNumOne.display();
+
+        Car carNumTwo = new Car();
+        carNumTwo.model = "Toyota Corolla 121";
+        carNumTwo.color = "Maroon";
+        carNumTwo.seats = 4;
+        carNumTwo.display();
     }
 }
